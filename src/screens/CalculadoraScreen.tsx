75cd31f70ff6cc2c3261c02baddfe4161952f762
Jhon D Orozco <jhon.orozco@javerianacali.edu.co>
2022-11-21 21:20:49 -0500

    [ADD] Configuración base Calculadora con styles

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import { styles } from '../theme/AppTheme';
+
+export const CalculadoraScreen = () => {
+  return (
+    <View> 
+        <Text style={styles.Texto}> Calculadora Screen</Text>
+    </View>
+  )
+}
