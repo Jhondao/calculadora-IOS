7ee79271548d4e14d4648e03506723e22e2d4178
Jhon D Orozco <jhon.orozco@javerianacali.edu.co>
2022-11-24 17:40:11 -0500

    [ADD] Estilo principal de la calculadora

--- src/components/BotonCalc.tsx
+++ src/components/BotonCalc.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { styles } from '../theme/AppTheme';
 
 interface Props {
-    texto: string,
-    color: string,
+    texto: string;
+    color?: string;
+    ancho?: boolean;
 }
 
 
-export const BotonCalc = ({ texto, color='#2D2D2D' }: Props) => {
+export const BotonCalc = ({ texto, color ='#2D2D2D', ancho=false }: Props) => {
   return (
-    <View style={{...styles.boton, backgroundColor: color}}>
-        <Text style={styles.botonText}>{texto}</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={{...styles.boton, backgroundColor: color, width:(ancho) ? 180 : 80 }}>
+          <Text style={{...styles.botonText, color: (color === '#9B9B9B') ? 'black' : 'white' }}>{texto}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
